e174b29994f880ea29b8884f3800ea53ed57d178
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2023-06-28 15:53:45 -0300

    remuevo polars

--- src/spectrum.rs
+++ src/spectrum.rs
@@ -1,5 +1,4 @@
 use csv::Writer;
-use polars::prelude::*;
 use std::error::Error;
 #[derive(Debug)]
 pub struct Spectrum {
@@ -16,25 +15,11 @@ impl Spectrum {
             transmittance_grid: tng,
         }
     }
-    pub fn to_csv(&self, dest_folder: &str) -> Result<String, Box<dyn Error>> {
-        let mut df: DataFrame = df!(
-            "wavenumber" => &self.wavenumber_grid,
-            "transmittance" => &self.transmittance_grid
-        )?;
-        let conv_filename = format!(
-            "./{}/{}.csv",
-            dest_folder,
-            &self.filename[..self.filename.len() - 4]
-        );
-        let mut file = std::fs::File::create(&conv_filename)?;
-        CsvWriter::new(&mut file).finish(&mut df)?;
-        Ok(String::from(&conv_filename))
-    }
 
-    pub fn to_csv_2(&self, dest_path: &str) -> Result<String, Box<dyn Error>> {
+    pub fn to_csv(&self, dest_folder: &str) -> Result<String, Box<dyn Error>> {
 	let conv_filename = format!(
 		"./{}/{}.csv",
-		dest_path,
+		dest_folder,
 		&self.filename[..self.filename.len() - 4]
 	    );
         let mut wtr = Writer::from_path(&conv_filename)?;
